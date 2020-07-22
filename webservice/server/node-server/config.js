0c20936037812f1ad01d5d92d5bfd882d3b3038d
krishnagee <krish.signing@gmail.com>
2020-07-22 20:08:16 +0200

    Merge pull request #1 from krishnagee/dependabot/npm_and_yarn/webservice/ui/eslint-4.19.1
    Bump eslint from 3.19.0 to 4.19.1 in /webservice/ui

--- webservice/server/node-server/config.js
+++ webservice/server/node-server/config.js
@@ -1,7 +1,23 @@
 module.exports = {
+    database: {
+        file: "./db/data.db"
+    },
+
+    restify: {
+        port: 8000,
+        host: "localhost",
+        options: {
+            name: "Intel Facial Recognition Server"
+        }
+    },
+    api: './api/swagger.json',
+
     mqtt: {
-        port: 3001,
+        backingStore: {
+
+        },
+        port: 1884,
         host: 'localhost',
-        http: {port: 3002, bundle: true, static: './'}  
+        http: {port: 3000, bundle: true, static: './'}  
     }
 }
